c2cc628dec966aa68e477484e19f16dd71c29018
OliverCWY <chen.wuyang@dhs.sg>
2020-04-15 00:51:11 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-mkdir /RemoteView
 cd /RemoteView
-docker run -d -p ${PORT}:8002 --security-opt seccomp=$(pwd)/chrome.json browsergapce:1.0
+npm test
--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
+cd /
 apt update
-apt upgrade
-apt-get remove docker docker-engine docker.io containerd runc -y
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-cd /RemoteView
-docker pull dosyago/browsergapce:1.0
-curl -o chrome.json https://raw.githubusercontent.com/dosyago/RemoteView/master/chrome.json
+apt -y upgrade
+apt install -y curl git wget nodejs npm
+git clone https://github.com/dosyago/RemoteView
+cd RemoteView
+./setup_machine.sh
